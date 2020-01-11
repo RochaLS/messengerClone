64dd6c06da5f9930d3d736a932c3b914f977af90
Lucas Rocha <ld.rocha@hotmail.com>
2020-01-10 16:50:50 -0800

    Initial Commit

---
+++ MessengerClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessengerClone
+//
+//  Created by Lucas Rocha on 2020-01-10.
+//  Copyright © 2020 Lucas Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
